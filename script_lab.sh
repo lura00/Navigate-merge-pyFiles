0f72c8eb809c99129b3ed3eef5a0979cc4bcca28
Ludvig-Rav <ludvig.skinstad@gmail.com>
2021-10-21 14:41:48 +0200

    First commit

---
+++ script_lab.sh
@@ -0,0 +1,48 @@
+# Tar in input från användare och räknar ut värdet
+# echo "Please feed me with two numbers"
+# read number1
+# read number2
+# echo "The sum of your two numbers is: "$(expr $number1 + $number2)""
+
+while true; do
+    pushd "$python_dir" > /dev/null
+    echo "Here is your working directory: $PWD"
+    echo ""
+    let count=0
+    for file in *
+    do
+        if [ -f $file ]; then
+            echo "This is files: $file"
+            let count=count+1
+        elif [ -d $file ]; then
+            echo "This is directories: $file"
+            let count=count+1
+        fi
+        if file --mime-type "$file" | grep .*.py; then
+            echo "It is python files!"
+            echo "Would you like to merge?"
+            read choice
+            if choice == "yes"; then
+                touch output.py
+                read merge1
+                read merge2
+                cat $merge1 $merge2 > output.py
+            else
+                echo "Loop continue"
+            fi
+        fi
+    done
+    echo ""
+    echo "Count: $count"
+
+    # Hittar in i val av mapp och sedan mergear .py-filer till en fil.py.
+
+    echo "What folder would you like to go ahead and open or type q to quit?"
+    read folder
+    if [[ $folder == 'q' ]]; then
+        echo "You are now leaving this program!"
+        break
+    fi
+    python_dir=$PWD/${folder}
+    cd ${python_dir}
+done
